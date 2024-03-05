6da1fbe1f8f9b4b24d9a68e3e0f0a0b2f5574124
Nick Lotz <nlotz909@gmail.com>
2024-03-04 23:08:54 -0600

    WIP: Add install and configure vbox plus mininet demo

---
+++ 06-what-is-openflow/install_configure_vbox_mininet.sh
@@ -0,0 +1,7 @@
+# Fasttrack repos
+apt install fasttrack-archive-keyring
+deb https://fasttrack.debian.net/debian-fasttrack/ bullseye-fasttrack main contrib
+deb https://fasttrack.debian.net/debian-fasttrack/ bullseye-backports-staging main contrib
+
+
+sudo apt-get install -y virtualbox
